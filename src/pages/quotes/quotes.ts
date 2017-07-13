573b7ea3624d643efcffdc7bfe60f3eb5a8622b5
robsonklima <robsonklima@gmail.com>
2017-07-12 22:33:35 -0300

    Alert buttons

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -23,7 +23,22 @@ export class QuotesPage implements OnInit {
     const alert = this.alertCtrl.create({
       title: 'Add Quote',
       subTitle: 'Are you sure?',
-      message: 'Are you sure you want to add que quote?'
+      message: 'Are you sure you want to add que quote?',
+      buttons:[
+        {
+          text: 'Yes, go ahead',
+          handler:() => {
+            console.log('Ok');
+          }
+        },
+        {
+          text: 'No, I changed my mind!',
+          role: 'cancel',
+          handler:() => {
+            console.log('Cancelled!');
+          }
+        }
+      ]
     });
 
     alert.present();
